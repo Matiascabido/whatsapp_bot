b0c9e73117aba597e87c0a22315fbccd06d44666
Matiascabido <cabidomatias@gmail.com>
2024-11-29 14:31:10 -0300

    fix an error

--- bases_bot_wts/flows/principal.js
+++ bases_bot_wts/flows/principal.js
@@ -3,7 +3,7 @@ const {  addKeyword } = require('@bot-whatsapp/bot')
 const { princialAnswer } = require('../utils/constants')
 
 const flowPrincipal = addKeyword(['la']).addAnswer([princialAnswer])
-    .addAction("Hace tu consulta", {capture: true }, async (ctx, ctxFn) => {
+    .addAnswer("Hace tu consulta", {capture: true }, async (ctx, ctxFn) => {
         const prompt = 'Responde Hola'
         const consulta = ctx.body
         let answer = await chat(prompt, consulta)
